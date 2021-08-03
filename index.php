7adc006649cf2eb971941bd3fa7467cb07a2eb86
rebeccafitzpatr <69223849+rebeccafitzpatr@users.noreply.github.com>
2021-08-04 09:32:28 +1200

    Create index.php

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en"
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <meta name="keywords" content="Epsom Girls Grammar, EGGS, Epsom Girls Grammar Coding Club, Coding, Coding Club"
+
+        <meta name="description" content="Epsom Girls Grammar Coding Club"
+        <meta name="author" content="Rebecca Fitzpatrick">
+
+        <title>EGGS Coding Club</title>
+
+        <!--to control the formatting-->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/stylesheet.css" rel="stylesheet">
+
+        <!--javascript plugins-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <link href="css/stylesheet.css" rel="stylesheet">
+
+
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/lightbox.js"></script>
+
+    </head>
+
+    <body>
+
+        <!-- navbar-->
+        <div class="navbar navbar-default" role="navigation">
+            <div class="navbar-header">
+
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+            <h1>EGGS Coding Club</h1>
+
+            </div><!-- end navbar-header div-->
+            
+            <div class="container">
+            <!--navigation links-->
+            
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-right">
+
+                    <li><a href="index.php">Home</a></li>
+
+                    <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">Resources... <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Video tutorials</a></li>
+                        <li><a href="#">Documents</a></li>
+                        <li><a href="#">Career education</a></li>
+                    </ul>
+                    </li>
+
+                    <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">Gallery <b class="caret"></b></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Gallery of Student work</a></li>
+                        <li><a href="contribute.php">Contribute work</a></li>
+                    </ul>
+                    </li>
+
+                    <li><a href="#" data-toggle="modal">Contact</a></li>
+                    </ul>
+
+                </div> <!-- navbarcollapse-->
+
+            </div>  <!--navigation container div-->
+        
+        </div>  <!-- end navbar navbar-default div -->
+
+
+        <!-- mainbody-->
+            <h1>Welcome to Eggs Coding Club</h1>
+
+        <!--footer-->
+
+
+        <!--content modal-->
+        
+
+
+
+    </body>
